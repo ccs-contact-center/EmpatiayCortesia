1aad0b48e23e7159cec4b5ec9d6b19a08e0fa159
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-27 13:53:54 -0500

    Update Actividad1View.js

--- src/views/temarioViews/Actividad1View.js
+++ src/views/temarioViews/Actividad1View.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react'
-import { CardBody, CardHeader, Col, Row } from 'reactstrap'
+import {
+  CardBody,
+  CardHeader,
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+
+  Input,
+} from 'reactstrap'
 //import SweetAlert from 'sweetalert2-react'
 import Actividad1 from './componentsExtras/dragdrog'
 
@@ -18,10 +28,183 @@ class Actividad1View extends Component {
             </CardHeader>
             <CardBody className="">
               <p>
-              De acuerdo al tipo de cliente, llena el paréntesis de la recomendación de manera de atenderle. 
-
+                De acuerdo al tipo de cliente, llena el paréntesis de la
+                recomendación de manera de atenderle.
               </p>
-              <Actividad1 />
+              <Form>
+                <div className="cajaA2" style={{ backgroundColor: '#d5d4d8' }}>
+                  <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label ml-2">
+                      <b>1.- ¿Qué es la empatía?</b>{' '}
+                      <span style={{ color: 'red' }}>*</span>
+                    </legend>
+                    <Col sm={12}>
+                      <FormGroup check>
+                        <Input
+                          type="text"
+                          name="respuesta1"
+                          id="respuesta1"
+                          placeholder="Tu respuesta"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </FormGroup>
+                </div>
+
+                <div
+                  className="cajaA2 mt-3"
+                  style={{ backgroundColor: '#d5d4d8' }}
+                >
+                  <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label ml-2">
+                      <b>2.- ¿Cuales son las tipos de empatia que existen?</b>{' '}
+                      <span style={{ color: 'red' }}>*</span>
+                    </legend>
+                    <Col sm={12}>
+                      <FormGroup check>
+                        <Input
+                          type="text"
+                          name="respuesta2"
+                          id="respuesta2"
+                          placeholder="Tu respuesta"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </FormGroup>
+                </div>
+
+                <div
+                  className="cajaA2 mt-3"
+                  style={{ backgroundColor: '#d5d4d8' }}
+                >
+                  <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label ml-2">
+                      <b>3.- ¿Por que es importante ser empáticos?</b>{' '}
+                      <span style={{ color: 'red' }}>*</span>
+                    </legend>
+                    <Col sm={12}>
+                      <FormGroup check>
+                        <Input
+                          type="text"
+                          name="respuesta3"
+                          id="respuesta3"
+                          placeholder="Tu respuesta"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </FormGroup>
+                </div>
+
+                <div
+                  className="cajaA2 mt-3"
+                  style={{ backgroundColor: '#d5d4d8' }}
+                >
+                  <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label ml-2">
+                      <b>
+                        4.- ¿Cuales son los rasgos mas importantes de una
+                        persona empatica?
+                      </b>{' '}
+                      <span style={{ color: 'red' }}>*</span>
+                    </legend>
+                    <Col sm={12}>
+                      <FormGroup check>
+                        <Input
+                          type="text"
+                          name="respuesta4"
+                          id="respuesta4"
+                          placeholder="Tu respuesta"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </FormGroup>
+                </div>
+
+                <div
+                  className="cajaA2 mt-3"
+                  style={{ backgroundColor: '#d5d4d8' }}
+                >
+                  <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label ml-2">
+                      <b>
+                        5.- Menciona tres acciones con las cuáles puedes mejorar
+                        tu empatía.
+                      </b>{' '}
+                      <span style={{ color: 'red' }}>*</span>
+                    </legend>
+                    <Col sm={12}>
+                      <FormGroup check>
+                        <Input
+                          type="text"
+                          name="respuesta5"
+                          id="respuesta5"
+                          placeholder="Tu respuesta"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </FormGroup>
+                </div>
+
+                <div
+                  className="cajaA2 mt-3"
+                  style={{ backgroundColor: '#d5d4d8' }}
+                >
+                  <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label ml-2">
+                      <b>
+                        6.- ¿Cuales son los 3 obstaculos mas frecuentes en la
+                        empatia?
+                      </b>{' '}
+                      <span style={{ color: 'red' }}>*</span>
+                    </legend>
+                    <Col sm={12}>
+                      <FormGroup check>
+                        <Input
+                          type="text"
+                          name="respuesta6"
+                          id="respuesta6"
+                          placeholder="Tu respuesta"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </FormGroup>
+                </div>
+
+                <div
+                  className="cajaA2 mt-3"
+                  style={{ backgroundColor: '#d5d4d8' }}
+                >
+                  <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label ml-2">
+                      <b>8. ¿Por qué es importante la voz en tu trabajo?</b>{' '}
+                      <span style={{ color: 'red' }}>*</span>
+                    </legend>
+                    <Col sm={12}>
+                      <FormGroup check>
+                        <Input
+                          type="text"
+                          name="text"
+                          id="respuesta"
+                          placeholder="Tu respuesta"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </FormGroup>
+                </div>
+
+                <br />
+
+                <FormGroup check row>
+                  <Col className="centrado-fila">
+                    <Button
+                      color="primary"
+                     
+                    >
+                      Enviar
+                    </Button>
+                  </Col>
+                </FormGroup>
+              </Form>
             </CardBody>
           </Col>
         </Row>
