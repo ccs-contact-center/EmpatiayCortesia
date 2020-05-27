8cc4569d8f3952354735bf8c383da6f2fdac0612
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-27 07:30:04 -0500

    curso Empatia y Cortesia

---
+++ src/_nav.js
@@ -0,0 +1,42 @@
+export default {
+  items: [
+    {
+      title: true,
+      name: 'Inicio',
+      class: 'text-center',
+    },
+    {
+      name: 'Objetivo',
+      icon: 'icon-tag',
+     
+    },
+
+    {
+      name: '¿Qué es un cliente dificil?',
+      icon: 'icon-tag',
+      
+    },
+
+    {
+      name: 'Tipos de clientes',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: 'Algunos consejos',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: 'EVALUACIÓN ',
+      icon: 'icon-puzzle',
+    
+    },
+
+   
+    {
+      name: 'DESPEDIDA',
+      icon: 'icon-tag',
+    },
+  ],
+}
